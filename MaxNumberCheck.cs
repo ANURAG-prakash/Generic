7424bd05aa43c5f507b81d8fee9763355b60a3b8
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-29 19:35:29 +0530

    Anurag Prakash Has Added UC2

---
+++ MaxNumberCheck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaximumNumberProject
+{
+    public class MaxNumberCheck
+    {
+
+
+        public static int MaximumIntergerNumber(int FirstNumber, int SecondNumber, int ThirdNumber)
+        {
+            if (FirstNumber.CompareTo(SecondNumber) > 0 && FirstNumber.CompareTo(ThirdNumber) > 0)
+            {
+                return FirstNumber;
+            }
+            if (SecondNumber.CompareTo(FirstNumber) > 0 && SecondNumber.CompareTo(ThirdNumber) > 0)
+            {
+                return SecondNumber;
+            }
+            if (ThirdNumber.CompareTo(FirstNumber) > 0 && ThirdNumber.CompareTo(SecondNumber) > 0)
+            {
+                return ThirdNumber;
+            }
+            throw new Exception("All Number Are Equal");
+        }
+        public static double MaximumFloatNumber(double FirstNumber, double SecondNumber, double ThirdNumber)
+        {
+            if (FirstNumber.CompareTo(SecondNumber) > 0 && FirstNumber.CompareTo(ThirdNumber) > 0)
+            {
+                return FirstNumber;
+            }
+            if (SecondNumber.CompareTo(FirstNumber) > 0 && SecondNumber.CompareTo(ThirdNumber) > 0)
+            {
+                return SecondNumber;
+            }
+            if (ThirdNumber.CompareTo(FirstNumber) > 0 && ThirdNumber.CompareTo(SecondNumber) > 0)
+            {
+                return ThirdNumber;
+            }
+            throw new Exception("All Number Are Equal");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FindMaximumNumberProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Number");
+            int output = MaxNumberCheck.MaximumIntergerNumber(11, 22, 33);
+            double number = MaxNumberCheck.MaximumFloatNumber(2.2, 3.3, 4.4);
+            Console.WriteLine(output);
+            Console.WriteLine(number);
+        }
+    }
+   
+}
