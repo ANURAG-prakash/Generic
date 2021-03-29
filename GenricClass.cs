44acd196c437d3d13cdc0768217dec5dec96ecdf
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-29 19:38:22 +0530

    Anurag Prakash has added UC4

---
+++ GenricClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaximumNumberProject
+{
+   public class GenericMaximum<T> where T : IComparable
+    {
+        public T[] value;
+        public GenericMaximum(T[] value)
+        {
+            this.value = value;
+        }
+        public T[] Sort(T[] value)
+        {
+            Array.Sort(value);
+            return value;
+        }
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(value);
+            return sorted_values[^1];
+        }
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum value is = " + max);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FindMaximumNumberProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Number");
+            int output = MaxNumberCheck.MaximumIntergerNumber(11, 22, 33);
+            double number = MaxNumberCheck.MaximumFloatNumber(2.2, 3.3, 4.4);
+            string value = MaxNumberCheck.MaximumStringValue("11", "22", "33");
+            Console.WriteLine(output);
+            Console.WriteLine(number);
+            Console.WriteLine(value);
+            int[] intArray = { 11, 22, 33 };
+            double[] doubleArray = { 2.2, 3.3,4.4 };
+            string[] stringArray = { "22", "33", "44" };
+            GenericMaximum<int> intgeneric = new GenericMaximum<int>(intArray);
+            intgeneric.PrintMaxValue();
+            GenericMaximum<double> doublegeneric = new GenericMaximum<double>(doubleArray);
+            doublegeneric.PrintMaxValue();
+            GenericMaximum<string> stringgeneric = new GenericMaximum<string>(stringArray);
+            stringgeneric.PrintMaxValue();
+        }
+    }
+   
+}
