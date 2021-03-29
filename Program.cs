9b681b5b66fc67233abca192752096bfe1c663a1
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-29 19:32:37 +0530

    Anurag prakash has added UC1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FindMaximumNumberProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Number");
+            int output = MaxNumberCheck.MaximumIntergerNumber(11, 22, 33);
+            Console.WriteLine(output);
+        }
+    }
+   
+}
